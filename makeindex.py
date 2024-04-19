f7597fbef80b4faef1af2b57909eff8264f13012
Mark Mendell <m.e.mendell@gmail.com>
2024-04-19 12:30:32 -0400

    Redo intro bio

--- makeindex.py
+++ makeindex.py
@@ -19,15 +19,42 @@ h2 {
 }
 
 #hi {
-	align-items: center;
-	display: flex;
 	margin-top: 16px;
 }
 
 @media (max-width: 550px) {
 #hi {
+	margin-bottom: 12px;
+}}
+
+@media (min-width: 550px) {
+#hi {
+	padding: 32px;
+}}
+
+#firstpair {
+	display: flex;
+}
+
+@media (max-width: 660px) {
+#firstpair {
 	flex-direction: column;
-	margin-bottom: -28px;
+}}
+
+@media (min-width: 660px) {
+#firstpair {
+	align-items: center;
+}}
+
+#picme {
+	align-self: center;
+	border-radius: 16px;
+	max-height: 200px;
+}
+
+@media (min-width: 660px) {
+#picme {
+	margin-right: 24px;
 }}
 
 #greeting {
@@ -36,8 +63,9 @@ h2 {
 	font-weight: bold;
 }
 
-@media (max-width: 550px) {
+@media (max-width: 660px) {
 #greeting {
+	margin-top: 24px;
 	text-align: center;
 }}
 
@@ -46,21 +74,15 @@ h2 {
 	display: flex;
 }
 
-@media (max-width: 550px) {
+@media (max-width: 660px) {
 #toolbar {
 	justify-content: center;
 }}
 
-#pitch {
+.pitch {
 	font-size: 18px;
 	line-height: 1.3;
 }
-
-@media (max-width: 550px) {
-#pitch {
-	display: block;
-	text-align: justify;
-}}
 
 .dash {
 	/*border: 1px dotted black;*/
@@ -125,7 +147,7 @@ a {
 @media (max-width: 550px) {
 .hproj {
 	flex-direction: column;
-}
+}}
 
 @media (min-width: 550px) {
 .hproj {
@@ -133,18 +155,34 @@ a {
 }}
 </style>
 <div id="hi">
-	<img src="1644558965003.jpg" style="border-radius:16px; max-height:200px">
-	<div style="display:flex; flex-direction:column; justify-content:center; padding:32px">
-		<span id="greeting">Hi, I’m Mark!</span>
-		<p><span id="pitch">I’m a generalist, as at home writing React components in TypeScript as I am writing optimized cryptography routines in C. Wherever you need some extra leverage, I’m happy to help.</span>
-		<div id="toolbar" style="">
-			<a class="licon" href="mailto:hire@mark.dev">hire@mark.dev</a>
-			<a class="licon" href="https://x.com/markdotdev" target="_blank"><svg width="15" height="15" viewbox="0 0 300 300" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/></svg></a>
-			<a class="licon" href="https://github.com/markmendell" target="_blank"><svg width="18" height="18" viewBox="0 0 64 64" id="i-github" xmlns="http://www.w3.org/2000/svg"> <path stroke-width="0" fill="#000000" d="M32 0 C14 0 0 14 0 32 0 53 19 62 22 62 24 62 24 61 24 60 L24 55 C17 57 14 53 13 50 13 50 13 49 11 47 10 46 6 44 10 44 13 44 15 48 15 48 18 52 22 51 24 50 24 48 26 46 26 46 18 45 12 42 12 31 12 27 13 24 15 22 15 22 13 18 15 13 15 13 20 13 24 17 27 15 37 15 40 17 44 13 49 13 49 13 51 20 49 22 49 22 51 24 52 27 52 31 52 42 45 45 38 46 39 47 40 49 40 52 L40 60 C40 61 40 62 42 62 45 62 64 53 64 32 64 14 50 0 32 0 Z" /></svg></a>
+	<div id="firstpair" style="">
+		<img id="picme" src="1644558965003.jpg">
+		<div>
+			<div id="greeting">Hi, I’m Mark!</div>
+			<span class="pitch">
+				<p>I'm an expert at every layer of the web stack:
+				<ul>
+					<li>TLS cryptography algorithms in C</li>
+					<li>deploying networked Linux VPSs</li>
+					<li>Python/Go/JS/C routes and SQL databases</li>
+					<li>custom HTML video players with canvas</li>
+					<li>TypeScript/React components</li>
+				</ul>
+			</span>
 		</div>
 	</div>
+	<span class="pitch">
+		<p>I've worked with Duolingo, Indeed, Thumbtack, and more on products used by millions of users.
+		<p>I've also built native cross-platform apps for macOS, Windows, Android, and iOS, and done graphics programming in Vulkan, Direct3D, and Metal.
+		<p>After graduating CMU and doing my rounds of the big tech companies, I found I could best leverage my agency working as an independent consultant. My strength lies in an empathetic approach to existing codebases and a love of simplicity and performance.
+	</span>
+	<div id="toolbar" style="">
+		<a class="licon" href="mailto:hire@mark.dev">hire@mark.dev</a>
+		<a class="licon" href="https://x.com/markdotdev" target="_blank"><svg width="15" height="15" viewbox="0 0 300 300" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"/></svg></a>
+		<a class="licon" href="https://github.com/markmendell" target="_blank"><svg width="18" height="18" viewBox="0 0 64 64" id="i-github" xmlns="http://www.w3.org/2000/svg"> <path stroke-width="0" fill="#000000" d="M32 0 C14 0 0 14 0 32 0 53 19 62 22 62 24 62 24 61 24 60 L24 55 C17 57 14 53 13 50 13 50 13 49 11 47 10 46 6 44 10 44 13 44 15 48 15 48 18 52 22 51 24 50 24 48 26 46 26 46 18 45 12 42 12 31 12 27 13 24 15 22 15 22 13 18 15 13 15 13 20 13 24 17 27 15 37 15 40 17 44 13 49 13 49 13 51 20 49 22 49 22 51 24 52 27 52 31 52 42 45 45 38 46 39 47 40 49 40 52 L40 60 C40 61 40 62 42 62 45 62 64 53 64 32 64 14 50 0 32 0 Z" /></svg></a>
+	</div>
 </div>
-<div style="background-color:#fffcec; border-radius:16px; box-shadow:rgba(149,157,165,0.2) 0px 8px 24px; display:flex; margin-top:32px; margin-bottom:-32px; padding:12px 24px">
+<div style="background-color:#fffcec; border-radius:16px; box-shadow:rgba(149,157,165,0.2) 0px 8px 24px; display:flex; margin-top:32px; padding:12px 24px">
 	<svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 379.51" width="16" height="16" style="flex-shrink:0; opacity:0.5"><path d="M212.27 33.98C131.02 56.52 78.14 103.65 64.99 185.67c-3.58 22.32 1.42 5.46 16.55-5.86 49.4-36.96 146.53-23.88 160.01 60.56 27.12 149.48-159.79 175.36-215.11 92.8-12.87-19.19-21.39-41.59-24.46-66.19C-11.35 159.99 43.48 64.7 139.8 19.94c17.82-8.28 36.6-14.76 56.81-19.51 10.12-2.05 17.47 3.46 20.86 12.77 2.87 7.95 3.85 16.72-5.2 20.78zm267.78 0c-81.25 22.54-134.14 69.67-147.28 151.69-3.58 22.32 1.42 5.46 16.55-5.86 49.4-36.96 146.53-23.88 160 60.56 27.13 149.48-159.78 175.36-215.1 92.8-12.87-19.19-21.39-41.59-24.46-66.19C256.43 159.99 311.25 64.7 407.58 19.94 425.4 11.66 444.17 5.18 464.39.43c10.12-2.05 17.47 3.46 20.86 12.77 2.87 7.95 3.85 16.72-5.2 20.78z"/></svg>
 	<blockquote style="font-style:italic; margin:14px">
 		Mark is an incredibly talented and efficient engineer. He has impeccable craft and can build sites at a very high bar at impressive speeds. He helped me bring my design portfolio to life, creating custom media controls, implementing unique transitions and more. What I love the most about Mark is his attention to detail. He will go the extra mile to get it perfect. He is a product designer's dream to work with!
